80d23329153115480f34eef62c6d2b57c6f8883d
Tomas Alvarez Chavez <93352642+Gakertz@users.noreply.github.com>
2026-08-09 15:25:42 -0400

    Implementa servicio de gestion de cursos

--- services/curso.service.js
+++ services/curso.service.js
@@ -8,11 +8,49 @@ import { Curso } from '../models/curso.model.js'
 
 // TODO: implementa las funciones que tus controllers necesiten. Por ejemplo:
 //   - listarCursos()            → Curso.find().populate('profesor').populate('alumnos')
+export const listarCursos = async () => {
+    return Curso.find()
+        .populate('profesor')
+        .populate('alumnos')
+    }
 //   - crearCurso(datos)
+export const crearCurso = async (datos) => {
+    return Curso.create(datos)
+}
 //   - buscarCurso(id)
+export const buscarCurso = async (id) => {
+    return Curso.findById(id)
+}
 //   - editarCurso(id, datos)
+export const editarCurso = async (id, datos) => {
+    return Curso.findByIdAndUpdate(
+        id,
+        datos,
+        {
+        new: true,
+        runValidators: true,
+        },
+    )
+}
 //   - borrarCurso(id)
+export const borrarCurso = async (id) => {
+    return Curso.findByIdAndDelete(id)
+}
 //   - cursosDelProfesor(profesorId)
+export const cursosDelProfesor = async (profesorId) => {
+    return Curso.find({
+        profesor: profesorId,
+    })
+        .populate('profesor')
+        .populate('alumnos')
+}
 //   - cursosDelAlumno(alumnoId)
-//
+export const cursosDelAlumno = async (alumnoId) => {
+    return Curso.find({
+        alumnos: alumnoId,
+    })
+        .populate('profesor')
+        .populate('alumnos')
+}
+
 // Piensa qué necesita cada ruta y crea solo lo que uses.
