75d9911227065ff95c87f31a3a697b20b9cabf59
Tomas Alvarez Chavez <93352642+Gakertz@users.noreply.github.com>
2026-08-09 12:29:12 -0400

    preparacion previa de proyecto

--- config/db.js
+++ config/db.js
@@ -10,7 +10,9 @@ import mongoose from 'mongoose'
 //    apunta a una base local. Nadie debe poder entrar a tu base desde tu repo.
 
 const MONGODB_URI =
-  'mongodb+srv://usuario-mongo:clave-secreta@cluster0.xxxxx.mongodb.net/plataforma'
+  //'mongodb+srv://usuario-mongo:clave-secreta@cluster0.xxxxx.mongodb.net/plataforma'
+  'mongodb+srv://curso3_api:curso3_api@cluster0.u6pcuvs.mongodb.net/plataforma'
+
 
 export const conectar = async () => {
   await mongoose.connect(MONGODB_URI)
